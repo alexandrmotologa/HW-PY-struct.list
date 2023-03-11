5ccb7904fc5af2181a764f8df7002a2011b5e1d3
Alexander Minaku <al.motologa@gmail.com>
2023-03-11 20:40:49 +0200

    HW

---
+++ app.py
@@ -0,0 +1,18 @@
+days =  [ "Mo", "Tu", "Wd", "Th", "Fr", "Sa", "Su" ]
+temps = [ 10.0,  9.0,  8.0,  0.0, -5.0, -2.0, 0.0  ]
+
+for i in range(7):
+    if temps[i] <= 0:
+        sign = "*"
+    else:
+        sign = " "
+    print(sign, days[i], temps[i])
+    
+i = 0
+while i < 7:
+    if temps[i] <= 0:
+        sign = "*"
+    else:
+        sign = " "
+    print(sign, days[i], temps[i])
+    i += 1
